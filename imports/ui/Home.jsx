e71b426468b46a5c3ecea9d08f492702c9bcd8b6
Maria Situmbeko <mariasitumbeko@gmail.com>
2018-05-05 11:56:15 +0200

    Adding Homepage image

--- imports/ui/Home.jsx
+++ imports/ui/Home.jsx
@@ -1,7 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import React, { Component } from 'react';
 import route from '/imports/routing/router.js';
-import { withTracker } from 'meteor/react-meteor-data';
 import Navbar from '/imports/ui/Navbar.jsx'
 
 export default class Home extends Component {
@@ -9,11 +8,13 @@ export default class Home extends Component {
     return(
       <div>
         <Navbar />
+        <div className="container-fluid">
+          <div className="image">
+            <img src="/images/homepage.jpg" />
+            <h2 id="adopt">Are you ready to adopt a pet?</h2>
+          </div>
+        </div>
       </div>
     )
   }
 }
-//
-// export default withTracker(() => {
-//   return {}
-// })(Home);
--- imports/ui/Navbar.jsx
+++ imports/ui/Navbar.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import route from '/imports/routing/router.js';
 
 export default class Navbar extends Component {
   render(){
     return(
       <div>
         <nav className="navbar navbar-expand-lg">
-          <span className="navbar-brand mb-0 h1" href="#">Navbar</span>
+          <span className="navbar-brand mb-0 h1" href="#"><img src="/images/logo.svg" />Pet Connection</span>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -15,11 +16,20 @@ export default class Navbar extends Component {
               <li className="nav-item active">
                 <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
               </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Pets</a>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Pets</a>
+                <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                  <a className="dropdown-item" href="/categories">Dogs</a>
+                  <a className="dropdown-item" href="/categories">Cats</a>
+                  <a className="dropdown-item" href="/categories">Birds</a>
+                </div>
               </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Pet Care</a>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Pet Care</a>
+                <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                  <a className="dropdown-item" href="#">Vet Services</a>
+                  <a className="dropdown-item" href="#">Pet Supplies</a>
+                </div>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="#">Home a Pet</a>
@@ -27,8 +37,12 @@ export default class Navbar extends Component {
               <li className="nav-item">
                 <a className="nav-link" href="#">Advertise</a>
               </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Get Involved</a>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Get Involved</a>
+                <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                  <a className="dropdown-item" href="#">Report a Stray</a>
+                  <a className="dropdown-item" href="#">Volunteer</a>
+                </div>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="#">Contact Us</a>
