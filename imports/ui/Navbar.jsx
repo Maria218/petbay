7d2ae86a206bc3a3e3eb597218d38beabe292e24
Maria Situmbeko <mariasitumbeko@gmail.com>
2018-05-05 10:35:05 +0200

    Adding navbar improvements

--- imports/ui/Navbar.jsx
+++ imports/ui/Navbar.jsx
@@ -16,10 +16,22 @@ export default class Navbar extends Component {
                 <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
+                <a className="nav-link" href="#">Pets</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Disabled</a>
+                <a className="nav-link" href="#">Pet Care</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Home a Pet</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Advertise</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Get Involved</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Contact Us</a>
               </li>
             </ul>
           </div>
