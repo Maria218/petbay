a35f77e8b017cb7da02464b09acf3e5d204e3dcf
Damiano Chintala <damianochintala@gmail.com>
2018-05-05 13:58:42 +0200

    Adding gallery template for Dog Categories

--- imports/routing/index.js
+++ imports/routing/index.js
@@ -1,4 +1,6 @@
 import route from './router.js';
 import Home from '/imports/ui/Home.jsx';
+import Categories from '/imports/ui/Categories.jsx'
 
 route('/', Home);
+route('/categories',Categories);
