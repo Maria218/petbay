b18ab6ed9890e7714ade7f04504e3d1abcda6b32
Maria Situmbeko <mariasitumbeko@gmail.com>
2018-05-06 16:26:02 +0200

    Adding a footer and other pet categories

--- imports/ui/Footer.jsx
+++ imports/ui/Footer.jsx
@@ -5,10 +5,12 @@ export default class Footer extends Component {
   render(){
     return(
       <div>
-        <footer class="page-footer font-small stylish-color-dark pt-4 mt-4">
-          <div class="col-md-4">
-            <h5 class="text-uppercase mb-4 mt-3 font-weight-bold">Footer Content</h5>
-            <p>Here you can use rows and columns here to organize your footer content. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        <footer className="">
+          <div className="container">
+            <div className="col-md-3 col-lg-4 col-xl-3 mb-4">
+              <h5 className="text-uppercase mb-4 mt-3 font-weight-bold">Footer Content</h5>
+              <p>Here you can use rows and columns here to organize your footer content. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
           </div>
         </footer>
       </div>
