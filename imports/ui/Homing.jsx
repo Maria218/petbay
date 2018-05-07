99774f91661e183a4a3465b9a46f6b785b6ad4f5
Damiano Chintala <damianochintala@gmail.com>
2018-05-07 10:19:49 +0200

    Adding Login page

--- imports/ui/Homing.jsx
+++ imports/ui/Homing.jsx
@@ -64,33 +64,33 @@ export default class Homing extends Component {
         </div>
           <form onSubmit = {this.getUserData}>
               <div className="form-group">
-                  <label className="col-form-label" for="formGroupExampleInput">Organization Name</label>
+                  <label className="col-form-label" htmlFor="formGroupExampleInput">Organization Name</label>
                   <input type="text" className="form-control" name="name" id="formGroupExampleInput" placeholder="Enter Name" required />
               </div>
               <div className="form-group">
-                  <label className="col-form-label" for="formGroupExampleInput2">Email Address</label>
+                  <label className="col-form-label" htmlFor="formGroupExampleInput2">Email Address</label>
                   <input type="email" className="form-control" name="email" id="formGroupExampleInput2" placeholder="Enter Email Address" required/>
               </div>
               <div className="form-group">
-                  <label className="col-form-label" for="formGroupExampleInput2">Contact Number</label>
+                  <label className="col-form-label" htmlFor="formGroupExampleInput2">Contact Number</label>
                   <input type="number" className="form-control" name="phoneNumber" id="formGroupExampleInput2" placeholder="Enter Phone Number" required/>
               </div>
               <div className="form-group">
-                  <label className="col-form-label" for="formGroupExampleInput2">Password</label>
+                  <label className="col-form-label" htmlFor="formGroupExampleInput2">Password</label>
                   <input type="password" className="form-control" name="password1" id="formGroupExampleInput2" placeholder="Enter Password" required/>
               </div>
               <div className="form-group">
-                  <label className="col-form-label" for="formGroupExampleInput2">Confirm Password</label>
+                  <label className="col-form-label" htmlFor="formGroupExampleInput2">Confirm Password</label>
                   <input type="password" className="form-control" name="password2" id="formGroupExampleInput2" placeholder="Enter Password Again" required/>
               </div>
               <div className="form-group">
-                  <label className="col-form-label" for="formGroupExampleInput2">Location</label>
+                  <label className="col-form-label" htmlFor="formGroupExampleInput2">Location</label>
                   <input type="text" className="form-control" name="location" id="formGroupExampleInput2" placeholder="Enter Town Name" required/>
               </div><br/>
               <div className="text-center">
               <button type="submit" className="btn btn-primary">Sign Up</button>
               </div>
-          </form>
+          </form><br/>
           <div className="text-center">
               <p>By Signing Up, You agree to our terms and conditions</p>
           </div>
--- imports/ui/LogIn.jsx
+++ imports/ui/LogIn.jsx
@@ -2,10 +2,11 @@ import { Meteor } from 'meteor/meteor';
 import React, { Component } from 'react';
 import route from '/imports/routing/router.js';
 import { withTracker } from 'meteor/react-meteor-data';
-import Navbar from '/imports/ui/Navbar.jsx'
+import Navbar from '/imports/ui/Navbar.jsx';
+import Footer from '/imports/ui/Footer.jsx';
 
 
-export default class Login extends Component{
+export default class LogIn extends Component{
     logUserIn = (e)=>{
         e.preventDefault();
         const email = e.target.email.value;
@@ -17,24 +18,30 @@ export default class Login extends Component{
       }
     render(){
         return(
+            <div>
+            <Navbar /><br/>
             <div className="row justify-content-center">
               <div className="col-md-4">
               <h4>Enter Your Details To Log In</h4>
                 <form>
                     <div className="form-group">
-                        <label className="col-form-label" for="formGroupExampleInput2">Email Address</label>
+                        <label className="col-form-label" htmlFor="formGroupExampleInput2">Email Address</label>
                         <input type="email" className="form-control" name="email" id="formGroupExampleInput2" placeholder="Enter Email Address" required/>
                     </div>
                     <div className="form-group">
-                        <label className="col-form-label" for="formGroupExampleInput2">Password</label>
+                        <label className="col-form-label" htmlFor="formGroupExampleInput2">Password</label>
                         <input type="password" className="form-control" name="password1" id="formGroupExampleInput2" placeholder="Enter Password" required/>
                     </div>
                     <br/>
+                    <div className="text-center">
                     <button type="submit" className="btn btn-primary">Login</button>
+                    </div>
                 </form>
                 <div>
                 </div>
             </div>
+            </div><br/>
+            <Footer />
             </div>
         )
     }
