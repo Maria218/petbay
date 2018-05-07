878ba65ca94654ffe5eee6c57435b6d1e9ac80ea
Maria Situmbeko <mariasitumbeko@gmail.com>
2018-05-07 13:16:46 +0200

    Adding a volunteer page

--- imports/ui/Volunteer.jsx
+++ imports/ui/Volunteer.jsx
@@ -14,6 +14,45 @@ export default class Volunteer extends Component {
         <h5 className="volunteer">Explore the list of organisations and help save our furry friends</h5>
         <br />
         <br />
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <img src="images/adoptioncenter.jpg" />
+            </div>
+            <div className="col-md-6">
+              <h5 className="top-middle">Lorem ipsum dolor</h5>
+              <p className="bottom-middle">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vestibulum mauris ut diam vulputate, nec scelerisque magna maximus.</p>
+              <button type="button" class="btn btn-outline-primary">Volunteer</button>
+            </div>
+          </div>
+          <br />
+          <br />
+          <div className="row">
+            <div className="col-md-6">
+              <img src="images/adoptioncenter.jpg" />
+            </div>
+            <div className="col-md-6">
+              <h5 className="top-middle">Lorem ipsum dolor</h5>
+              <p className="bottom-middle">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vestibulum mauris ut diam vulputate, nec scelerisque magna maximus.</p>
+              <button type="button" class="btn btn-outline-primary">Volunteer</button>
+            </div>
+          </div>
+          <br />
+          <br />
+          <div className="row">
+            <div className="col-md-6">
+              <img src="images/adoptioncenter.jpg" />
+            </div>
+            <div className="col-md-6">
+              <h5 className="top-middle">Lorem ipsum dolor</h5>
+              <p className="bottom-middle">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vestibulum mauris ut diam vulputate, nec scelerisque magna maximus.</p>
+              <button type="button" class="btn btn-outline-primary text-center">Volunteer</button>
+            </div>
+          </div>
+          <i class="fa fa-angle-right text-center"></i>
+        </div>
+        <br />
+        <br />
         <Footer />
       </div>
     )
