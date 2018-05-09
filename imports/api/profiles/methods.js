8c896047eac0c0547b80b55d5a6c20f9e7998071
Damiano Chintala <damianochintala@gmail.com>
2018-05-09 08:41:27 +0200

    Changing Profile files

--- imports/api/profiles/methods.js
+++ imports/api/profiles/methods.js
@@ -1,13 +1,4 @@
 import {Meteor} from 'meteor/meteor';
-<<<<<<< HEAD:imports/api/profiles/methods.jsx
 
 
-=======
-import { Accounts } from 'meteor/accounts-base';
- 
-Meteor.methods({
-    'users.create':(user)=>{
-        Accounts.createUser(user);
-    }
-})
->>>>>>> 0f44de0a2b1bf4afa9b06983faea2f038eac3295:imports/api/profiles/methods.js
+
