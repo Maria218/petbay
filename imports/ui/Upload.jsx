de8456ae6dea4c9ea00957f1383e009918af6fb0
Damiano Chintala <damianochintala@gmail.com>
2018-05-10 09:06:52 +0200

    Adding Form to upload component

--- imports/ui/Upload.jsx
+++ imports/ui/Upload.jsx
@@ -13,21 +13,26 @@ export default class Uploads extends Component {
         <form>
         <div class="row">
           <div class="col">
-          <label for="inputEmail4">Email</label>
+          <label for="inputEmail4">Pet Name</label>
             <input type="text" class="form-control" placeholder="First name"/>
           </div>
           <div class="col">
-          <label for="inputEmail4">Email</label>
+          <label for="inputEmail4">Age</label>
             <input type="text" class="form-control" placeholder="Last name"/>
           </div>
         </div>
       <div class="row">
         <div class="col">
-        <label for="inputEmail4">Email</label>
-          <input type="text" class="form-control" placeholder="First name"/>
+        <label class="mr-sm-2" for="inlineFormCustomSelect">Gender</label>
+        <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
+          <option selected>Select Gender</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
         </div>
         <div class="col">
-        <label for="inputEmail4">Email</label>
+        <label for="inputEmail4">Breed</label>
           <input type="text" class="form-control" placeholder="Last name"/>
         </div>
       </div>
