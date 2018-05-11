665f21037773dc7c55b31575e06d20b39412f2f6
Damiano Chintala <damianochintala@gmail.com>
2018-05-11 14:09:07 +0200

    Adding Pet upload collection

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,5 +1,7 @@
 // All API methods and collections that needs to be used on the server should be imported here
 
 import { Meteor } from 'meteor/meteor';
-import '../../api/profiles/methods';
+import '../../api/profiles/methods.js';
+import '../../api/profiles/server/publications.js';
+
 
--- imports/ui/Upload.jsx
+++ imports/ui/Upload.jsx
@@ -7,6 +7,45 @@ export default class Uploads extends Component {
   constructor(props){
     super(props)
   }
+  getUserData =(e) =>{
+    e.preventDefault();
+    const {target} = e;
+    const name = target.name.value;
+    const email = target.email.value;
+    const phone = target.phone.value;
+    const password = target.password.value;
+    const password2 = target.password2.value;
+
+   if(password.trim()!==password2.trim()){
+     this.setState({
+       error: "Passwords do not match"
+     })
+     return;
+   };
+   if(password.length <=6
+   ){
+     this.setState({
+       error2: "Password too short"
+     })
+     return;
+   }
+   const user = {
+     email,
+     password,
+
+     profile: {
+       name,
+       phone,
+     },
+     createdAt: new Date()
+   }
+   Accounts.createUser(user,error=>{
+     error ? console.log(error.reason) : console.log("Account Created Successfully")
+   }) ;
+
+  route.go('/dashboard');
+
+  }
 
   render(){
     return(
@@ -31,21 +70,22 @@ export default class Uploads extends Component {
           <form className="upload">
             <div className="form-group">
               <label htmlFor="exampleFormControlFile1">Upload Pet Image</label>
-              <input type="file" className="form-control-file" id="exampleFormControlFile1" />
+              <input type="file" name="image" className="form-control-file" id="exampleFormControlFile1" />
             </div>
           <div className="row">
             <div className="col">
-              <label htmlFor="inputEmail4">Pet Name</label><input type="text" className="form-control" placeholder="Pet name" required/>
+              <label htmlFor="inputEmail4">Pet Name</label>
+              <input type="text" className="form-control" name="petName"  placeholder="Pet name" required/>
             </div>
             <div className="col">
             <label htmlFor="inputEmail4">Pet Age</label>
-              <input type="text" className="form-control" placeholder="Pet name" required/>
+              <input type="text" className="form-control" name="age"  placeholder="Pet age" required/>
             </div>
           </div><br />
         <div className="row">
           <div className="col">
           <label className="mr-sm-2" htmlFor="inlineFormCustomSelect">Pet Gender</label>
-          <select className="custom-select mr-sm-2" id="inlineFormCustomSelect" required>
+          <select className="custom-select mr-sm-2" id="inlineFormCustomSelect" name="gender"  required>
             <option defaultValue>Select Gender</option>
             <option value="1">female</option>
             <option value="2">male</option>
@@ -53,23 +93,24 @@ export default class Uploads extends Component {
           </div>
           <div className="col">
           <label htmlFor="inputEmail4">Pet Breed</label>
-            <input type="text" className="form-control" placeholder="Last name" required/>
+            <input type="text" className="form-control" name="breed"  placeholder="Breed" required/>
           </div>
         </div><br />
         <div className="row">
           <div className="col">
-            <label htmlFor="inputEmail4">Pet Location</label><input type="text" className="form-control" placeholder="Pet name" required/>
+            <label htmlFor="inputEmail4">Pet Location</label>
+            <input type="text" className="form-control" name="location"  placeholder="Pet location" required/>
           </div>
           <div className="col">
           <label htmlFor="inputEmail4">Health</label>
-            <input type="text" className="form-control" placeholder="Pet name" required/>
+            <input type="text" className="form-control" name="health" placeholder="Health status" required/>
           </div>
         </div><br />
         <div className="row">
           <div className="col-md-3"></div>
           <div className="col-md-6 form-group describe">
             <label htmlFor="exampleFormControlTextarea1">PET DESCRIPTION</label>
-            <textarea className="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
+            <textarea className="form-control" id="exampleFormControlTextarea1" name="description"  rows="5"></textarea>
           </div>
           <div className="col-md-3"></div>
         </div>
