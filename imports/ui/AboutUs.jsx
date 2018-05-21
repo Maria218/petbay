1b022bf8f204045926a8bad362349625dc5fa907
Maria Situmbeko <mariasitumbeko@gmail.com>
2018-05-21 13:57:59 +0200

    Editing about us page

--- imports/ui/AboutUs.jsx
+++ imports/ui/AboutUs.jsx
@@ -10,29 +10,24 @@ export default class AboutUs extends Component{
     return(
       <div>
         <Navbar about={' active'}/>
-        <div>
-          <img src="/images/cat4.jpg" />
+        <div className="header-img">
+          <img src="/images/aboutUsCat.jpg" />
           <h1 id="adopt">About the Team</h1>
         </div>
         <div className="container" style={{marginTop:20+"px"}}>
-        <div className="row justify-content-center">
-        <div className="col-md-10">
-        <h2 style={{textAlign:"center"}}>We Are Pet Lovers Trying To Make A Difference</h2><br/>
-
-        <p>We use the power of the Internet to connect adopters and buyers with pets from shelters, individuals,and stores. And help pets go from alone to having a new home. We're working to help the good people at shelters and rescue groups find homes for their pets as well as allow pet stores and individuals sell their pets on our platform. But we don't stop there. We also provide useful and informative information on the human/companion animal relationship to help keep pets healthy and permanently in their loving homes. Check our <a href="/vets" >Pet Care</a> section for  useful pet related articles</p>
-        </div>
-        </div>
-        <div className="row justify-content-center">
-        <div className="col-md-8">
-        <h1 style={{textAlign:"center"}}>Meet our Developer Team</h1><br/>
-        <p>
-        We are a talented team of Emerging Developers with complementary skillsets. In our team you can find  developers,designers, data architects, system administrators and admin stuff.
-        </p>
-        <p>
-        Due to the strong technical background which is in the heart of our team,  All web can practically take over and deliver any technical challenge, ranging from the development of simple web applications to highly demanding web tools, while the team members have the knowledge and business thinking to identify and utilize the value that is generated through the technical capabilities that are provided by existing or newly developed software.
-        </p>
-        </div>
-        </div>
+          <div className="row justify-content-center">
+            <div className="col-md-8">
+              <h2 className="aboutUs" style={{textAlign:"center"}}>We Are Pet Lovers Trying To Make A Difference</h2><br/>
+              <p>We use the power of the Internet to connect adopters and buyers with pets from shelters, individuals, and stores. And help pets go from alone to having a new home. We're working to help the good people at shelters and rescue groups find homes for their pets as well as allow pet stores and individuals sell their pets on our platform. But we don't stop there. We also provide useful and informative information on the human/companion animal relationship to help keep pets healthy and permanently in their loving homes. Check our <a href="/vets" >Pet Care</a> section for  useful pet related articles</p>
+            </div>
+          </div>
+          <div className="row justify-content-center">
+            <div className="col-md-8">
+              <h1 className="aboutUs" style={{textAlign:"center"}}>Meet our Developer Team</h1><br/>
+              <p>We are a talented team of Emerging Developers with complementary skill sets. In our team you can find  developers,designers, data architects, system administrators and admin stuff.</p>
+              <p>Due to the strong technical background which is in the heart of our team,  All web can practically take over and deliver any technical challenge, ranging from the development of simple web applications to highly demanding web tools, while the team members have the knowledge and business thinking to identify and utilize the value that is generated through the technical capabilities that are provided by existing or newly developed software.</p>
+            </div>
+          </div>
         <div style={{marginTop:20+"px"}}>
         <section class="row  placeholders">
         <div class="col-md-6 col-sm-6 placeholder text-center">
